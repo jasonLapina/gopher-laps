9704d5ef6a4ad5afb56f1242d6843a6d613a500e
jasonLapina <lapina.jason@gmail.com>
2025-06-01 21:41:48 +0800

    create registrations table

--- database/database.go
+++ database/database.go
@@ -49,4 +49,19 @@ CREATE TABLE IF NOT EXISTS events (
 		panic(err)
 	}
 
+	createRegistrationsTable := `
+CREATE TABLE IF NOT EXISTS registrations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event_id INTEGER,
+    user_id INTEGER,
+	FOREIGN KEY (event_id) REFERENCES events(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)
+`
+
+	_, err = DB.Exec(createRegistrationsTable)
+	if err != nil {
+		panic(err)
+	}
+
 }
